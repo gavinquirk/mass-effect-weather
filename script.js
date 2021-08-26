eeee890132a3e512c1070f7fbdf9fb88c213fb21
Gavin Quirk <gavinquirk@gmail.com>
2021-08-26 14:55:26 -0700

    weather api setup, basic query

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log('working...')
+
+// Elements
+const thermostat = document.querySelector('#thermostat');
+
+// API Query
+const weatherApiKey = config.weatherApiKey
+
+const userLocation = 'Carlsbad'
+
+const queryUrl = `http://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${userLocation}&aqi=no`
+
+const weatherData = fetch(queryUrl)
+  .then(response => response.json())
+  .then(data => console.log(data));
